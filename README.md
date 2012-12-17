e964453996901e96760e19f0f6808739618cdd66
Samir Hussain <shussain@credil.org>
2012-12-17 14:04:00 -0500

    Removing trailing white space

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ This script is a Pomodoro timing tool (http://en.wikipedia.org/wiki/Pomodoro_Tec
 
 It will use pynotify to display notificaiton to user to either take a break or to get back to work - depending on arguments passed to it in the command line
 
-## Requirements 
+## Requirements
 This script requires:
     * pynotify
 
 ## Usage
 
 
-To work for 25 minutes, enter: python pomodoro-notify     
+To work for 25 minutes, enter: python pomodoro-notify
 To keep track of a 5 minute break, enter: python pomodoro-notify -b
 
--- pomodoro-notify.py
+++ pomodoro-notify.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 # This script is a Pomodoro timing tool (http://en.wikipedia.org/wiki/Pomodoro_Technique).
-# It will use pynotify to display notificaiton to user to either take a break or to 
+# It will use pynotify to display notificaiton to user to either take a break or to
 # get back to work - depending on arguments passed to it in the command line
 #
-# To work for 25 minutes, enter: ./pomodoro-notify     
+# To work for 25 minutes, enter: ./pomodoro-notify
 # To keep track of a 5 minute break, enter: ./pomodoro-notify -b
 import sys
 from optparse import OptionParser
@@ -16,7 +16,7 @@ except:
     sys.stderr.write("ERROR: pynotify not found. Application will not work without it.\n")
     sys.exit(1)
 
-BREAKTIME = 300 # 5 minutes (in seconds) 
+BREAKTIME = 300 # 5 minutes (in seconds)
 WORKTIME = 1500 # 25 minutes (in seconds)
 
 VERSION = 0.1   # Version of the software
@@ -24,7 +24,7 @@ VERSION = 0.1   # Version of the software
 # Parse options that user has entered
 def parse_options():
     usage = """\
-pomdoro-notify.py 
+pomdoro-notify.py
         [options]
 """
     parser = OptionParser(usage=usage)
@@ -49,7 +49,7 @@ def main():
     if (options.version):
         print "Version %2.2f" % (VERSION)
         sys.exit(1)
-   
+
     # Sleep till notification is needed.
     sleeptime = BREAKTIME if (options.breaktime) else WORKTIME
     sleep(sleeptime)
